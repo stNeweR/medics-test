60a1c48073e6eaed06f2c4aea8e852782e14376b
Сарафанников Егор <shyguy096com@gmail.com>
2024-05-27 14:11:34 +0700

    Добавление пользователя через фронтенд

--- frontend/src/pages/PeopleView.vue
+++ frontend/src/pages/PeopleView.vue
@@ -1,4 +1,14 @@
 <template>
+  <h1>Добавить пользователя</h1>
+  <form @submit.prevent="addPerson" class="flex flex-col w-1/3">
+    <label for="last_name">Фамилия:</label>
+    <input type="text" id="last_name" name="last_name" class="border border-black" v-model="last_name">
+    <label for="first_name">Имя:</label>
+    <input type="text" id="first_name" name="first_name" class="border border-black" v-model="first_name">
+    <label for="second_name">Отчество:</label>
+    <input type="text" id="second_name" name="second_name" class="border border-black"  v-model="second_name">
+    <button>Добавить</button>
+  </form>
   <h1>Контакты пользователей</h1>
   <table class="min-w-full text-left">
     <thead>
@@ -16,15 +26,15 @@
         <td>{{ contact.first_name }} {{ contact.second_name }} {{ contact.last_name }}</td>
         <td>
           <ul class="flex gap-2">
-            <li v-for="phone in contact.phones " :key="phone"> {{ phone }} </li>
+            <li v-for="phone in contact.phones " :key="phone"> {{ phone.phone_number }} </li>
             <li v-if="contact.phones.length === 0">
-              <button>Добавить номер</button>
+              <router-link :to="{ name: 'person', params: {id: contact.id}}">Добавить номер</router-link>
             </li>
           </ul>
         </td>
         <td>{{ contact.created_at }}</td>
         <td>
-          <router-link :to="`/peoples/${contact.id}`">Подробнее</router-link>
+          <router-link :to="{ name: 'person', params: {id: contact.id}}">Подробнее</router-link>
         </td>
       </tr>
     </tbody>
@@ -32,17 +42,50 @@
 </template>
 
 <script setup>
-import {onMounted, ref} from "vue";
+import {onMounted, reactive, ref} from "vue";
 import axios from "axios";
 
 const contacts = ref([])
+const first_name = ref('')
+const second_name = ref('')
+const last_name = ref('')
 
-onMounted( async () => {
+const getPeopleData = async () => {
   try {
     const response = await axios.get('http://localhost:8080/api/peoples');
     contacts.value = response.data.peoples
   } catch (error) {
     console.error(error)
   }
-})
+}
+
+onMounted(getPeopleData)
+
+const addPerson = async () => {
+  const data = {
+    'first_name': first_name.value,
+    'second_name': second_name.value,
+    'last_name': last_name.value,
+  }
+  try {
+    const response = await fetch(`http://localhost:8080/api/peoples/store`, {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    })
+
+    getPeopleData()
+    last_name.value = ''
+    first_name.value = ''
+    second_name.value = ''
+
+    console.log(response)
+  } catch (error) {
+    console.error(error)
+  }
+}
 </script>
--- frontend/src/pages/PersonView.vue
+++ frontend/src/pages/PersonView.vue
@@ -7,12 +7,22 @@
       <p><b>Фамилия:</b> {{ person.last_name }}</p>
       <p class=""><b>Номера телефонов: </b>
       <ul class="">
-        <li v-for="phone in person.phones">
-          {{ phone }}
+        <li class="flex gap-2" v-for="phone in person.phones">
+          {{ phone.phone_number }}
+          <form @submit.prevent="deletePhone(phone.id)">
+            <button>Удалить номер</button>
+          </form>
         </li>
       </ul>
       </p>
     </div>
+    <h1><b>Добавить номер</b></h1>
+    <form @submit.prevent="addPhone" class="flex flex-col">
+      <label for="phone_number">Введите номер для пользователя (вот в таком формате +7 (902) 560-95-54)</label>
+      <input type="tel" name="phone_number" required id="phone_number" class="border w-1/5" v-model="phoneNumber">
+      <button>Добавить</button>
+    </form>
+    <div v-if="hasError" class="text-red-500 mt-2">{{ errorMessage }}</div>
   </div>
 
 </template>
@@ -23,17 +33,56 @@ import {onMounted, reactive, ref} from "vue";
 import axios from "axios";
 
 const personId = reactive(useRoute().params.id)
-
+const hasError = ref(false);
+const errorMessage = ref('');
 const person = ref([])
+const phoneNumber = ref('');
 
-onMounted(async () => {
+
+const getPersonData = async () => {
   try {
     const response = await axios.get(`http://localhost:8080/api/peoples/show?id=${personId}`)
     person.value = response.data['peoples'][0]
-    console.log(response.data)
   } catch (error) {
     console.error(error)
   }
-})
-</script>
+}
 
+onMounted(getPersonData)
+
+const addPhone = async  () => {
+  try {
+    await fetch(`http://localhost:8080/api/peoples/phones?id=${personId}/`, {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        phone_number: phoneNumber.value
+      })
+    })
+
+    getPersonData();
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const deletePhone = async (id) => {
+  try {
+    const response = await fetch(`http://localhost:8080/api/peoples/phones/delete?id=${id}`, {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    getPersonData();
+  } catch (error) {
+    console.error(error)
+  }
+}
+</script>
